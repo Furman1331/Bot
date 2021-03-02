5912d8c1e2e4c62faaa6d2b48c64cb611352d44e
Furman1331 <michalfurmanczak@gmail.com>
2021-03-02 23:42:51 +0100

    JAPIERDOLE

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 const settings = require('./settings.json');
 const prefix = settings.prefix || "!";
 
-client.warn = require("./warn.json");
+client.info = require("./warn.json");
 
 client.on('ready', async () => {
     console.log(`Pomyślnie uruchomiono bota ${settings.name}!`);
@@ -24,23 +24,23 @@ client.on('message', async message => {
     const command = args.shift().toLowerCase();
 
     if (command === 'help') {
-        message.author.send(`Dostępne komedy: \`\`\`!help - Dostępne komendy\n!faq - Najczęściej zadawane pytania.\`\`\``).then(message => setTimeout(() => {message.delete();}, 4000));
+        message.author.send(`Dostępne komedy: \`\`\`!help - Dostępne komendy\n!faq - Najczęściej zadawane pytania.\`\`\``).then(message => setTimeout(() => {message.delete();}, settings.timeToDelete));
     }
     else if (command === 'faq'){
-        message.author.send(`Obecnie nie dostępne`).then(messages => setTimeout(() => {messages.delete();}, 4000));
+        message.author.send(`Obecnie nie dostępne`).then(messages => setTimeout(() => {messages.delete();}, settings.timeToDelete));
     }
     else if(command === 'clear') {
         if(message.member.hasPermission("MANAGE_MESSAGES")) {
             const amount = parseInt(args[0]);
             if(!args.length) {
-                return message.channel.send(`Nie podano ilości, ${message.author}!`).then(messages => setTimeout(() => { messages.delete(); message.delete()}, 4000));
+                return message.channel.send(`Nie podano ilości, ${message.author}!`).then(messages => setTimeout(() => { messages.delete(); message.delete()}, settings.timeToDelete));
             }
             else if(!isNaN(amount)) {
                 message.channel.bulkDelete(amount + 1).catch(error => console.log(error.stack)).then(messages => console.log(`Wyczyszczono wiadomości na kanale ${message.channel.name}, ilość : ${amount} !`)).catch(console.error);
-                return message.channel.send(`Pomyślnie usunięto wiadomości, ${message.author}!`).then(messages => setTimeout(() => { messages.delete()}, 4000));
+                return message.channel.send(`Pomyślnie usunięto wiadomości, ${message.author}!`).then(messages => setTimeout(() => { messages.delete()}, settings.timeToDelete));
             }
             else {
-                return message.channel.send(`Argument musi być liczbą, ${message.author}!`).then(messages => setTimeout(() => { messages.delete(), message.delete()}, 4000));
+                return message.channel.send(`Argument musi być liczbą, ${message.author}!`).then(messages => setTimeout(() => { messages.delete(), message.delete()}, settings.timeToDelete));
             }
         }
     }
@@ -49,23 +49,26 @@ client.on('message', async message => {
             const user = getUserFromMention(args[0]);
             const reason = args[1] && args[1].trim() || "Brak powodu";
             if(!user) {
-                return message.channel.send(`Nie ma takiego użytkownika, ${message.author}!`).then(messages => setTimeout(() => { messages.delete(), message.delete()}, 4000));
+                return message.channel.send(`Nie ma takiego użytkownika, ${message.author}!`).then(messages => setTimeout(() => { messages.delete(), message.delete()}, settings.timeToDelete));
             }
 
-            if(!client.warn[message.author.id]) {
-                console.log(`Hello add warn`);
-                client.warn[message.author.id] = {
+            if(!client.info[message.guild.id]) {
+                client.info[message.guild.id] = {};
+            }
+
+            if(!client.info[message.guild.id][message.author.id]) {
+                client.info[message.guild.id][message.author.id] = {
                     "warn": 0
                 }
             }
 
-            let warnCount = client.warn[message.author.id].warn = client.warn[message.author.id].warn + 1
-            fs.writeFile("./warn.json", JSON.stringify(client.warn, null, 4), err => {
+            let warnCount = client.info[message.guild.id][message.author.id].warn = client.info[message.guild.id][message.author.id].warn + 1
+            fs.writeFile("./warn.json", JSON.stringify(client.info, null, 4), err => {
                 if(err) throw err;
             });
 
             return message.channel.send(`Pomyślnie nadano warna dla \`${user.username}\`! To jest jego \`${warnCount}\` warn, ${message.author}`)
-                .then(messages => setTimeout(() => { messages.delete(), message.delete()}, 4000))
+                .then(messages => setTimeout(() => { messages.delete(), message.delete()}, settings.timeToDelete))
                 .then(any => {client.channels.cache.get(settings.warnLogs).send(`Administrator \|\|${message.author}\|\|, nadał warna graczu \`${user.username}\` i to jest jego \`${warnCount}\` warn!`)})
                 .then(private => user.send(`Otrzymałeś warna od administratora \|\|${message.author}\|\|, Powód warna : \`${reason}\`! To jest twój \`${warnCount}\` warn, ${user}`));
         }
